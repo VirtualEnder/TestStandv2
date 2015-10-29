1eb5df55602e7f92e6080f5c2dc43bea1237386f
VirtualEnder <virtualender@gmail.com>
2015-10-29 16:36:24 -0400

    Cleanup

--- Thrust_Stand_Test/Thrust_Stand_Test.ino
+++ Thrust_Stand_Test/Thrust_Stand_Test.ino
@@ -18,10 +18,10 @@
 #define MAGSENS true      // Using Magnetic RPM sensor?
 #define OPTISENS false    // Using Magnetic RPM sensor?
 #define POLES 14          // Number of magnetic poles in test motor.
-#define OVERSAMPLING 64   // Analog oversampling multiplier
 #define MINTHROTTLE 1000  // Low end of ESC calibrated range
 #define MAXTHROTTLE 2000  // High end of ESC calibrated range
 #define MINCOMMAND 980    // Value sent to ESC when test isn't running.
+#define OVERSAMPLING 64   // Analog oversampling multiplier
 #define VSCALE 26         // Scale factor for Voltage divider.
 #define CSCALE 100        // Scale factor for current sensor.
 #define LSCALE -700       // Scale factor for load cell amplifier.
