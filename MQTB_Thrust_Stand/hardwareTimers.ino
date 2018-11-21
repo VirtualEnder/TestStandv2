f56d833a0dafc1dd3222e4d37940a88a9d0932f7
Ryan Harrell <virtualender@gmail.com>
2018-11-21 16:07:00 -0500

    Fix PWM output for PWM0 G1 P2

--- MQTB_Thrust_Stand/hardwareTimers.ino
+++ MQTB_Thrust_Stand/hardwareTimers.ino
@@ -52,16 +52,15 @@ void initPWMOut () {
       SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);       //Enable control of PWM module 0
       SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);      //Enable control of GPIO B
   
-      GPIOPinConfigure(GPIO_PB4_M0PWM2);                // Map PB6 to PWM0 G0, OP 0
+      GPIOPinConfigure(GPIO_PB4_M0PWM2);                // Map PB4 to PWM0 G1, OP 2
       GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_4);      //Configure PB4 as PWM
        
       uint64_t PWMPeriod = (SysCtlClockGet () / ESCRATE);
-      PWMGenConfigure(PWM0_BASE, PWM_GEN_0, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);    //Configure PWM0 G0 as UP/DOWN counter with no sync of updates
-      PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, PWMPeriod - 1);    //Set period of PWM0 G0
-      PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, MINCOMMAND*10);    //Set duty cycle of PWM0 G0
-      PWMOutputState(PWM0_BASE, PWM_OUT_0_BIT | PWM_OUT_1_BIT , true);    //Enable OP 0,1 on PWM0 G0
-      PWMGenEnable(PWM0_BASE, PWM_GEN_0);    //Enable PWM0, G0*/
-    
+      PWMGenConfigure(PWM0_BASE, PWM_GEN_1, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);    //Configure PWM0 G0 as UP/DOWN counter with no sync of updates
+      PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, PWMPeriod - 1);    //Set period of PWM0 G1
+      PWMPulseWidthSet(PWM0_BASE, PWM_OUT_2, MINCOMMAND*10);    //Set duty cycle of PWM0 G1 OP 2
+      PWMOutputState(PWM0_BASE, PWM_OUT_2_BIT , true);    //Enable OP 2 on PWM0 G1
+      PWMGenEnable(PWM0_BASE, PWM_GEN_1);    //Enable PWM0, G1 */
 }
 
 void updatePWM(unsigned pulseWidth) {
